f0438091a8fa5c7226ca3a20f0bafc9c6ff07ba9
Shachar Sharon <ssharon@redhat.com>
2024-02-12 11:03:17 +0200

    vfs_ceph_ll: new VFS-module bridge using cephfs' low-level APIs
    Initial (empty) skeleton for new VFS-module which uses libcephfs'
    low-level APIs instead of the higher-level APIs used by current
    vfs_ceph.c module. Using the low-level APIs allows more fine-grained
    functionality:
    
      - Explicit user-credentials (libcephfs' UserPerms) per call.
      - Improved resource consumption.
      - Inodes caching.
    
    Signed-off-by: Shachar Sharon <ssharon@redhat.com>

---
+++ source3/modules/vfs_ceph_ll.c
@@ -0,0 +1,39 @@
+/*
+ * VFS module to bridge Samba with CephFS, using libcephfs low-level APIs.
+ *
+ * Copyright (C) 2024, Shachar Sharon <ssharon@redhat.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+#include "includes.h"
+#include "auth.h"
+#include "smbd/smbd.h"
+#include "system/filesys.h"
+#include "modules/posixacl_xattr.h"
+#include "lib/util/tevent_unix.h"
+#include <cephfs/libcephfs.h>
+
+
+/* VFS ceph_ll hooks */
+static struct vfs_fn_pointers vfs_ceph_fns = {
+
+};
+
+static_decl_vfs;
+NTSTATUS vfs_ceph_ll_init(TALLOC_CTX *ctx)
+{
+	return smb_register_vfs(SMB_VFS_INTERFACE_VERSION,
+				"ceph_ll",
+				&vfs_ceph_fns);
+}
