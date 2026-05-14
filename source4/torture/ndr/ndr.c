a838abaa0a6a6a1db16b2df50993692c1a584ace
Shachar Sharon <ssharon@redhat.com>
2026-05-14 16:06:09 +0300

    s4/torture: Test registry with V4 format
    Add test-suite for registry.tdb using V4 (IDL/NDR) on-disk format.
    
    Signed-off-by: Shachar Sharon <ssharon@redhat.com>

--- source4/torture/ndr/ndr.c
+++ source4/torture/ndr/ndr.c
@@ -786,6 +786,7 @@ struct torture_suite *torture_local_ndr(TALLOC_CTX *mem_ctx)
 	torture_suite_add_suite(suite, ndr_charset_suite(suite));
 	torture_suite_add_suite(suite, ndr_svcctl_suite(suite));
 	torture_suite_add_suite(suite, ndr_ODJ_suite(suite));
+	torture_suite_add_suite(suite, ndr_registry_suite(suite));
 
 	torture_suite_add_simple_test(suite, "string terminator",
 				      test_check_string_terminator);
