4470ecb049bf1d5aee45e4ee56007eca07fe0966
Shachar Sharon <ssharon@redhat.com>
2026-07-22 10:53:03 +0300

    messaging: ndr format for MSG_PROFILE
    Signed-off-by: Shachar Sharon <ssharon@redhat.com>

--- librpc/ndr/ndr_messaging.c
+++ librpc/ndr/ndr_messaging.c
@@ -126,3 +126,37 @@ enum ndr_err_code messaging_debuglevel_v1_pull(TALLOC_CTX *mem_ctx,
 
 	return NDR_ERR_SUCCESS;
 }
+
+enum ndr_err_code messaging_profile_v1_push(TALLOC_CTX *mem_ctx,
+					    struct messaging_profile_v1 *msg,
+					    DATA_BLOB *blob)
+{
+	msg->version = MESSAGING_PROFILE_VERSION_CURRENT;
+	return ndr_push_struct_blob(blob,
+				    mem_ctx,
+				    msg,
+				    (ndr_push_flags_fn_t)
+					    ndr_push_messaging_profile_v1);
+}
+
+enum ndr_err_code messaging_profile_v1_pull(TALLOC_CTX *mem_ctx,
+					    const DATA_BLOB *blob,
+					    struct messaging_profile_v1 *msg)
+{
+	enum ndr_err_code ndr_err;
+
+	ndr_err = ndr_pull_struct_blob(blob,
+				       mem_ctx,
+				       msg,
+				       (ndr_pull_flags_fn_t)
+					       ndr_pull_messaging_profile_v1);
+	if (!NDR_ERR_CODE_IS_SUCCESS(ndr_err)) {
+		return ndr_err;
+	}
+
+	if (msg->version != MESSAGING_PROFILE_VERSION_CURRENT) {
+		return NDR_ERR_VALIDATE;
+	}
+
+	return NDR_ERR_SUCCESS;
+}
