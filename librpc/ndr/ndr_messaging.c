322b6b9a69f0ba5030a8c478d386da877c5db588
Shachar Sharon <ssharon@redhat.com>
2026-08-24 12:52:38 +0300

    messaging: ndr format for MSG_DAEMON_READY_FD
    Signed-off-by: Shachar Sharon <ssharon@redhat.com>

--- librpc/ndr/ndr_messaging.c
+++ librpc/ndr/ndr_messaging.c
@@ -485,6 +485,42 @@ enum ndr_err_code messaging_ringbuf_log_pull(TALLOC_CTX *mem_ctx,
 	return NDR_ERR_SUCCESS;
 }
 
+enum ndr_err_code messaging_daemon_ready_fd_push(
+	TALLOC_CTX *mem_ctx,
+	struct messaging_daemon_ready_fd *msg,
+	DATA_BLOB *blob)
+{
+	msg->version = MESSAGING_DAEMON_READY_FD_VERSION_CURRENT;
+	return ndr_push_struct_blob(
+		blob,
+		mem_ctx,
+		msg,
+		(ndr_push_flags_fn_t)ndr_push_messaging_daemon_ready_fd);
+}
+
+enum ndr_err_code messaging_daemon_ready_fd_pull(
+	TALLOC_CTX *mem_ctx,
+	const DATA_BLOB *blob,
+	struct messaging_daemon_ready_fd *msg)
+{
+	enum ndr_err_code ndr_err;
+
+	ndr_err = ndr_pull_struct_blob(
+		blob,
+		mem_ctx,
+		msg,
+		(ndr_pull_flags_fn_t)ndr_pull_messaging_daemon_ready_fd);
+	if (!NDR_ERR_CODE_IS_SUCCESS(ndr_err)) {
+		return ndr_err;
+	}
+
+	if (msg->version != MESSAGING_DAEMON_READY_FD_VERSION_CURRENT) {
+		return NDR_ERR_VALIDATE;
+	}
+
+	return NDR_ERR_SUCCESS;
+}
+
 enum ndr_err_code messaging_id_cache_delete_push(
 	TALLOC_CTX *mem_ctx,
 	struct messaging_id_cache_delete *msg,
