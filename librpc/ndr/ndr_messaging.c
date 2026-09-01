36023aaca53d628de28009d87d0c7f7cdcc11b4b
Shachar Sharon <ssharon@redhat.com>
2026-09-01 18:14:56 +0300

    messaging: ndr format for MSG_REQ_PROFILELEVEL/MSG_PROFILELEVEL
    Signed-off-by: Shachar Sharon <ssharon@redhat.com>

--- librpc/ndr/ndr_messaging.c
+++ librpc/ndr/ndr_messaging.c
@@ -160,3 +160,75 @@ enum ndr_err_code messaging_profile_v1_pull(TALLOC_CTX *mem_ctx,
 
 	return NDR_ERR_SUCCESS;
 }
+
+enum ndr_err_code messaging_req_profilelevel_v1_push(
+	TALLOC_CTX *mem_ctx,
+	struct messaging_req_profilelevel_v1 *msg,
+	DATA_BLOB *blob)
+{
+	msg->version = MESSAGING_PROFILELEVEL_VERSION_CURRENT;
+	return ndr_push_struct_blob(
+		blob,
+		mem_ctx,
+		msg,
+		(ndr_push_flags_fn_t)ndr_push_messaging_req_profilelevel_v1);
+}
+
+enum ndr_err_code messaging_req_profilelevel_v1_pull(
+	TALLOC_CTX *mem_ctx,
+	const DATA_BLOB *blob,
+	struct messaging_req_profilelevel_v1 *msg)
+{
+	enum ndr_err_code ndr_err;
+
+	ndr_err = ndr_pull_struct_blob(
+		blob,
+		mem_ctx,
+		msg,
+		(ndr_pull_flags_fn_t)ndr_pull_messaging_req_profilelevel_v1);
+	if (!NDR_ERR_CODE_IS_SUCCESS(ndr_err)) {
+		return ndr_err;
+	}
+
+	if (msg->version != MESSAGING_PROFILELEVEL_VERSION_CURRENT) {
+		return NDR_ERR_VALIDATE;
+	}
+
+	return NDR_ERR_SUCCESS;
+}
+
+enum ndr_err_code messaging_profilelevel_v1_push(
+	TALLOC_CTX *mem_ctx,
+	struct messaging_profilelevel_v1 *msg,
+	DATA_BLOB *blob)
+{
+	msg->version = MESSAGING_PROFILELEVEL_VERSION_CURRENT;
+	return ndr_push_struct_blob(blob,
+				    mem_ctx,
+				    msg,
+				    (ndr_push_flags_fn_t)
+					    ndr_push_messaging_profilelevel_v1);
+}
+
+enum ndr_err_code messaging_profilelevel_v1_pull(
+	TALLOC_CTX *mem_ctx,
+	const DATA_BLOB *blob,
+	struct messaging_profilelevel_v1 *msg)
+{
+	enum ndr_err_code ndr_err;
+
+	ndr_err = ndr_pull_struct_blob(
+		blob,
+		mem_ctx,
+		msg,
+		(ndr_pull_flags_fn_t)ndr_pull_messaging_profilelevel_v1);
+	if (!NDR_ERR_CODE_IS_SUCCESS(ndr_err)) {
+		return ndr_err;
+	}
+
+	if (msg->version != MESSAGING_PROFILELEVEL_VERSION_CURRENT) {
+		return NDR_ERR_VALIDATE;
+	}
+
+	return NDR_ERR_SUCCESS;
+}
